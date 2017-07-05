3a34005fdb9fd657be7641f88e528a6ab80b39db
lstolowski <lstolowski@vauelogic.one>
2017-07-05 11:10:43 +0200

    aliases tweaked

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,9 +1,10 @@
 alias zshconfig="vim ~/.zshrc"
-#alias vncserver='x11vnc -rfbauth .x11vnc/passwd'
-alias vncserver='x11vnc -display :0 -many -rfbauth ~/.x11vnc/passwd'
 alias youtube-dl-mp3='youtube-dl --extract-audio --add-metadata --audio-format mp3'
 alias cls='clear'
 alias diffmeld='meld'
-alias rm='rm -i'
-alias ntpsync='sudo ntpdate ntp.ubuntu.com'
-alias gdrive='grive'
+alias rm='rm -i
+
+alias gst='git status'
+alias gpull='git pull'
+alias gcommit='git commit'
+alias gpush='git push''
